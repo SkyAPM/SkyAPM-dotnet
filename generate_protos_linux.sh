ec6c0d2efcc6633b0df4b2bd525fd16c7b2d68b3
Lemon <liuhaoyang1221@hotmail.com>
2018-02-14 10:54:47 +0800

    Update generate_protos_linux.sh

--- generate_protos_linux.sh
+++ generate_protos_linux.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
+dotnet restore 
+
 PROTOC=~/.nuget/packages/google.protobuf.tools/3.5.1/tools/linux_x64/protoc
 PLUGIN=~/.nuget/packages/grpc.tools/1.9.0/tools/linux_x64/grpc_csharp_plugin
-
-dotnet restore
 
 $PROTOC -I protos --csharp_out src/SkyWalking.NetworkProtocol  protos/ApplicationRegisterService.proto --grpc_out src/SkyWalking.NetworkProtocol --plugin=protoc-gen-grpc=$PLUGIN
 $PROTOC -I protos --csharp_out src/SkyWalking.NetworkProtocol  protos/Common.proto --grpc_out src/SkyWalking.NetworkProtocol --plugin=protoc-gen-grpc=$PLUGIN
