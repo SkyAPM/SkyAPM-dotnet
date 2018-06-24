33b1e1edad7b82b30d9405f2d7fbb3761b83c85a
liuhaoyang <liuhaoyang1221@hotmail.com>
2018-06-24 19:14:46 +0800

    Add TLS and Token support

---
+++ src/SkyWalking.Core/Remote/Authentication/AuthenticationInterceptors.cs
@@ -0,0 +1,28 @@
+﻿using Grpc.Core;
+using SkyWalking.Utils;
+
+namespace SkyWalking.Remote.Authentication
+{
+    internal class AuthenticationInterceptors
+    {
+        private const string header = "authentication";
+        
+        public static AsyncAuthInterceptor CreateAuthInterceptor(string token)
+        {
+            return (context, metadata) =>
+            {
+                var entry = CreateTokenHeader(token);
+                if (entry != null)
+                {
+                    metadata.Add(entry);
+                }    
+                return TaskUtils.CompletedTask;
+            };
+        }
+
+        private static Metadata.Entry CreateTokenHeader(string token)
+        {
+            return string.IsNullOrEmpty(token) ? null : new Metadata.Entry(header, token);
+        }
+    }
+}
---
+++ src/SkyWalking.Core/Remote/GrpcChannelBuilder.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using SkyWalking.Remote.Authentication;
+
+namespace SkyWalking.Remote
+{
+    internal class GrpcChannelBuilder
+    {
+        private string _token;
+
+        private string _server;
+
+        private string _rootCertificatePath;
+
+        public GrpcChannelBuilder WithAuthenticationToken(string token)
+        {
+            _token = token;
+            return this;
+        }
+
+        public GrpcChannelBuilder WithServer(string server)
+        {
+            _server = server;
+            return this;
+        }
+
+        public GrpcChannelBuilder WithCredential(string rootCertificatePath)
+        {
+            _rootCertificatePath = rootCertificatePath;
+            return this;
+        }
+
+        public Channel Build()
+        {
+            return new Channel(_server, GetCredentials());
+        }
+
+        private ChannelCredentials GetCredentials()
+        {
+            if (_rootCertificatePath != null)
+            {
+                var authInterceptor = AuthenticationInterceptors.CreateAuthInterceptor(_token);
+                return ChannelCredentials.Create(new SslCredentials(), CallCredentials.FromInterceptor(authInterceptor));
+            }
+            return ChannelCredentials.Insecure;
+        }
+    }
+}
