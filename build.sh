5053f693d85c9c9da0696f1920b0d9d399513cec
liuhaoyang <liuhaoyang@terminus.io>
2018-07-07 00:18:47 +0800

    Add build_coreclr option

--- build.sh
+++ build.sh
@@ -8,12 +8,37 @@ OBJ_DIR=`echo ${ROOT_DIR}/obj`
 PROFILER_DIR=`echo ${SRC_DIR}/profiler`
 CORECLR_DIR=`echo ${SRC_DIR}/coreclr`
 
+BUILD_TYPE="debug"
+BUILD_CORECLR=false
+
+while [ $# != 0 ]; do
+    case $1 in
+        debug | -debug)
+            BUILD_TYPE=$1
+        ;;
+        release | -release)
+            BUILD_TYPE=$1
+        ;;
+        build_coreclr | -build_coreclr)
+            BUILD_CORECLR=true
+        ;;
+        esac
+    shift
+done
+
 echo "ROOT_DIR=${ROOT_DIR}"
 echo "BUILD_DIR=${BUILD_DIR}"
 echo "SRC_DIR=${SRC_DIR}"
 
+if [ $BUILD_CORECLR == true ]
+then
+cd ${CORECLR_DIR}
+./build.sh ${BUILD_TYPE}
+fi
+
+cd ${ROOT_DIR}
 mkdir -p ${OBJ_DIR}/profiler && cd ${OBJ_DIR}/profiler
-cmake -DCMAKE_BUILD_TYPE=Release ${PROFILER_DIR}
+cmake -DCMAKE_BUILD_TYPE=${BUILD_TYPE} ${PROFILER_DIR}
 make
 
 cd ${ROOT_DIR}
