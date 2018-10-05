2067cc01aa43c9c059a15c7f339f29cd19cfe351
liuhaoyang <liuhaoyang1221@hotmail.com>
2018-10-05 20:41:28 +0800

    Add SkyWalking.DotNet.CLI for SkyWalking dotnet global tool

---
+++ cli/SkyWalking.DotNet.CLI/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace SkyWalking.DotNet.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var current =  Directory.GetCurrentDirectory();
+            Console.WriteLine(current);
+        }
+    }
+}
