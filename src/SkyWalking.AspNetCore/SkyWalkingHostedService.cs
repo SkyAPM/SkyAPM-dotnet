abc79bea9c42b0ae543e297752d7b59712c029ee
Savorboard <yangxiaodong1214@126.com>
2018-12-05 22:00:07 +0800

    upgrade deps nuget packages.

--- src/SkyWalking.AspNetCore/SkyWalkingHostedService.cs
+++ src/SkyWalking.AspNetCore/SkyWalkingHostedService.cs
@@ -20,7 +20,6 @@ using System;
 using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
 using SkyWalking.Boot;
