32e6d4368975dae15115f1506970e2945d0cafc5
Ahoo Wang <ahoowang@qq.com>
2019-03-08 18:21:47 +0800

    add SkyApm.Diagnostics.SmartSql project

---
+++ src/SkyApm.Diagnostics.SmartSql/SmartSqlTracingDiagnosticProcessor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SmartSql;
+using SmartSql.Diagnostics;
+
+namespace SkyApm.Diagnostics.SmartSql
+{
+    public class SmartSqlTracingDiagnosticProcessor : ITracingDiagnosticProcessor
+    {
+        public string ListenerName => SmartSqlDiagnosticListenerExtensions.SMART_SQL_DIAGNOSTIC_LISTENER;
+
+
+    }
+}
