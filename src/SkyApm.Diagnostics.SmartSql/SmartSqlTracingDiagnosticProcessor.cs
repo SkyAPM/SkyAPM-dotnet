e8efb908c1ee70c1041ee3a873d4c3514770e55d
Ahoo Wang <ahoowang@qq.com>
2019-03-10 21:53:05 +0800

    add SmartSqlTracingDiagnosticProcessor

--- src/SkyApm.Diagnostics.SmartSql/SmartSqlTracingDiagnosticProcessor.cs
+++ src/SkyApm.Diagnostics.SmartSql/SmartSqlTracingDiagnosticProcessor.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using SkyApm.Tracing;
 using SmartSql;
 using SmartSql.Diagnostics;
 
@@ -10,6 +11,55 @@ namespace SkyApm.Diagnostics.SmartSql
     {
         public string ListenerName => SmartSqlDiagnosticListenerExtensions.SMART_SQL_DIAGNOSTIC_LISTENER;
 
+        private readonly ITracingContext _tracingContext;
+        private readonly ILocalSegmentContextAccessor _localSegmentContextAccessor;
+        private readonly IExitSegmentContextAccessor _exitSegmentContextAccessor;
 
+        public SmartSqlTracingDiagnosticProcessor(ITracingContext tracingContext,
+            ILocalSegmentContextAccessor localSegmentContextAccessor,
+            IExitSegmentContextAccessor exitSegmentContextAccessor)
+        {
+            _tracingContext = tracingContext;
+            _localSegmentContextAccessor = localSegmentContextAccessor;
+            _exitSegmentContextAccessor = exitSegmentContextAccessor;
+        }
+
+        private static string ResolveOperationName(ExecutionContext executionContext)
+        {
+            return executionContext.Request.FullSqlId != "." ?
+                executionContext.Request.FullSqlId : executionContext.Request.ExecutionType.ToString();
+        }
+
+        [DiagnosticName(SmartSqlDiagnosticListenerExtensions.SMART_SQL_BEFORE_DB_SESSION_INVOKE)]
+        public void BeforeDbSessionInvoke([Object]DbSessionInvokeBeforeEventData eventData)
+        {
+            var context = _tracingContext.CreateExitSegmentContext(ResolveOperationName(eventData.ExecutionContext), "unkown");
+            context.Span.SpanLayer = Tracing.Segments.SpanLayer.DB;
+            context.Span.Component = Common.Components.SMART_SQL;
+            context.Span.AddTag(Common.Tags.DB_TYPE, "Sql");
+        }
+        [DiagnosticName(SmartSqlDiagnosticListenerExtensions.SMART_SQL_AFTER_DB_SESSION_INVOKE)]
+        public void AfterDbSessionInvoke([Object]DbSessionInvokeAfterEventData eventData)
+        {
+            var context = _exitSegmentContextAccessor.Context;
+            if (context != null)
+            {
+                context.Span.AddTag("from_cache", eventData.ExecutionContext.Result.FromCache);
+                context.Span.Peer = new Common.StringOrIntValue(eventData.ExecutionContext.DbSession.Connection?.DataSource);
+                context.Span.AddTag(Common.Tags.DB_INSTANCE, eventData.ExecutionContext.DbSession.Connection?.Database);
+                context.Span.AddTag(Common.Tags.DB_STATEMENT, eventData.ExecutionContext.Request.RealSql);
+                _tracingContext.Release(context);
+            }
+        }
+        [DiagnosticName(SmartSqlDiagnosticListenerExtensions.SMART_SQL_ERROR_DB_SESSION_INVOKE)]
+        public void ErrorDbSessionInvoke([Object]DbSessionInvokeErrorEventData eventData)
+        {
+            var context = _exitSegmentContextAccessor.Context;
+            if (context != null)
+            {
+                context.Span.ErrorOccurred(eventData.Exception);
+                _tracingContext.Release(context);
+            }
+        }
     }
 }
