8c4eb2c4d977940f1b3956e3ad08b835a5ff87ba
caozhiyuan <568022847@qq.com>
2019-04-03 21:07:55 +0800

    gacutil just use System.EnterpriseServices.Internal.GacInstall or GacRemove

--- tools/gacutil/Program.cs
+++ tools/gacutil/Program.cs
@@ -17,48 +17,12 @@
  */
 
 using System;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+using System.EnterpriseServices.Internal;
 
 namespace gacutil
 {
     class Program
     {
-        [DllImport("Fusion.dll", CharSet = CharSet.Auto)]
-        internal static extern int CreateAssemblyCache(out IAssemblyCache ppAsmCache, uint dwReserved);
-
-        /// <summary>
-        /// code from https://github.com/Microsoft/referencesource/blob/master/System.Web/Configuration/IAssemblyCache.cs
-        /// </summary>
-        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-        [Guid("e707dcde-d1cd-11d2-bab9-00c04f8eceae")]
-        [ComImport]
-        internal interface IAssemblyCache
-        {
-            [MethodImpl(MethodImplOptions.PreserveSig)]
-            int UninstallAssembly(
-                uint dwFlags,
-                [MarshalAs(UnmanagedType.LPWStr)] string pszAssemblyName,
-                IntPtr pvReserved,
-                out uint pulDisposition);
-
-            [MethodImpl(MethodImplOptions.PreserveSig)]
-            int InstallAssembly(uint dwFlags, 
-                [MarshalAs(UnmanagedType.LPWStr)] string pszManifestFilePath,
-                IntPtr pvReserved);
-        }
-
-        public enum UninstallStatus
-        {
-            None = 0,
-            Uninstalled = 1,
-            StillInUse = 2,
-            AlreadyUninstalled = 3,
-            DeletePending = 4,
-            HasInstallReferences = 5,
-            ReferenceNotFound = 6
-        }
-
         static void Main(string[] args)
         {
             if (args.Length < 2)
@@ -70,17 +34,17 @@ namespace gacutil
             try
             {
                 var mode = args[0].ToLower();
-                CreateAssemblyCache(out var ppAsmCache, 0U);
+                var publish = new Publish();
                 switch (mode)
                 {
                     case "/i":
-                        ppAsmCache.InstallAssembly(0, args[1], (IntPtr)0);
+                        publish.GacInstall(args[1]);
                         Console.WriteLine($"gacutil /i {args[1]}");
                         break;
                     case "/u":
                     {
-                        ppAsmCache.UninstallAssembly(0U, args[1], (IntPtr) 0, out var status);
-                        Console.WriteLine($"gacutil /u {args[1]}, status:{(UninstallStatus)status}");
+                        publish.GacRemove(args[1]);  // maybe fail , no return uninstall status.
+                        Console.WriteLine($"gacutil /u {args[1]}");
                     }
                         break;
                     default:
