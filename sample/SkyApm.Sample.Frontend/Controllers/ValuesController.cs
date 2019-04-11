523df1daf4007d352bd8a54d337466038c635ee1
caozhiyuan <568022847@qq.com>
2019-04-11 22:24:28 +0800

    add miss await

--- sample/SkyApm.Sample.Frontend/Controllers/ValuesController.cs
+++ sample/SkyApm.Sample.Frontend/Controllers/ValuesController.cs
@@ -20,7 +20,7 @@ namespace SkyApm.Sample.Frontend.Controllers
         public async Task<string> Get(int id)
         {
             var client = new HttpClient();
-            Task.WhenAll(client.GetAsync("http://localhost:5002/api/delay/2000"),
+            await Task.WhenAll(client.GetAsync("http://localhost:5002/api/delay/2000"),
                 client.GetAsync("http://localhost:5002/api/values"),
                 client.GetAsync("http://localhost:5002/api/delay/200"));
             return await client.GetStringAsync("http://localhost:5002/api/delay/100");
