78902f58b7c29fe8e02e3ff8ce4bb0e37b593344
他大舅他二舅都是他舅 <help@itsvse.com>
2020-11-11 16:20:14 +0800

    Update ConfigSectionExtensions.cs

--- src/SkyApm.Utilities.Configuration/ConfigSectionExtensions.cs
+++ src/SkyApm.Utilities.Configuration/ConfigSectionExtensions.cs
@@ -16,39 +16,21 @@
  *
  */
 
-using System;
-using System.Collections.Generic;
-using System.IO;
-using Microsoft.Extensions.Configuration;
+using System.Linq;
 using SkyApm.Config;
 
 namespace SkyApm.Utilities.Configuration
 {
-    internal static class ConfigurationBuilderExtensions
+    public static class ConfigSectionExtensions
     {
-        public static IConfigurationBuilder AddSkyWalkingDefaultConfig(this IConfigurationBuilder builder, IConfiguration configuration)
+        public static string GetSections(this ConfigAttribute config)
         {
-            var defaultLogFile = Path.Combine("logs", "skyapm-{Date}.log");
-            var defaultConfig = new Dictionary<string, string>
+            if (config.Sections == null || config.Sections.Length == 0)
             {
-                {"SkyWalking:Namespace", configuration.GetSection("SkyWalking:Namespace").Value ?? string.Empty },
-                {"SkyWalking:ServiceName", configuration.GetSection("SkyWalking:ServiceName").Value ?? "My_Service" },
-                {"Skywalking:ServiceInstanceName", configuration.GetSection("SkyWalking:ServiceInstanceName").Value ?? Guid.NewGuid().ToString("N") },
-                {"SkyWalking:HeaderVersions:0", configuration.GetSection("SkyWalking:HeaderVersions:0").Value ?? HeaderVersions.SW8 },
-                {"SkyWalking:Sampling:SamplePer3Secs", configuration.GetSection("SkyWalking:Sampling:SamplePer3Secs").Value ?? "-1" },
-                {"SkyWalking:Sampling:Percentage", configuration.GetSection("SkyWalking:Sampling:Percentage").Value ?? "-1" },
-                {"SkyWalking:Logging:Level",  configuration.GetSection("SkyWalking:Logging:Level").Value ?? "Information" },
-                {"SkyWalking:Logging:FilePath", configuration.GetSection("SkyWalking:Logging:FilePath").Value ?? defaultLogFile },
-                {"SkyWalking:Transport:Interval", configuration.GetSection("SkyWalking:Transport:Interval").Value ?? "3000" },
-                {"SkyWalking:Transport:ProtocolVersion", configuration.GetSection("SkyWalking:Transport:ProtocolVersion").Value ?? ProtocolVersions.V8 },
-                {"SkyWalking:Transport:QueueSize", configuration.GetSection("SkyWalking:Transport:QueueSize").Value ?? "30000" },
-                {"SkyWalking:Transport:BatchSize", configuration.GetSection("SkyWalking:Transport:BatchSize").Value ?? "3000" },
-                {"SkyWalking:Transport:gRPC:Servers",configuration.GetSection("SkyWalking:Transport:gRPC:Servers").Value ?? "localhost:11800" },
-                {"SkyWalking:Transport:gRPC:Timeout",configuration.GetSection("SkyWalking:Transport:gRPC:Timeout").Value ?? "10000" },
-                {"SkyWalking:Transport:gRPC:ReportTimeout",configuration.GetSection("SkyWalking:Transport:gRPC:ReportTimeout").Value ?? "600000" },
-                {"SkyWalking:Transport:gRPC:ConnectTimeout",configuration.GetSection("SkyWalking:Transport:gRPC:ConnectTimeout").Value ?? "10000" }
-            };
-            return builder.AddInMemoryCollection(defaultConfig);
+                return null;
+            }
+
+            return config.Sections.Length == 1 ? config.Sections[0] : config.Sections.Aggregate((x, y) => x + ":" + y);
         }
     }
 }
