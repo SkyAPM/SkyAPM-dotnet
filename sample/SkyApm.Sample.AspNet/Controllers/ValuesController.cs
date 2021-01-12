bf22a722612b9c00471beefb77186502e25d461e
Savorboard <yangxiaodong1214@126.com>
2021-01-12 10:06:47 +0800

    Revert "Remove support for asp.net of .net framework."
    This reverts commit 2e1c97a8822cc5d8a21962e9c0dd3d008f2a3119.

---
+++ sample/SkyApm.Sample.AspNet/Controllers/ValuesController.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using SkyApm.Agent.AspNet;
+
+namespace SkyApm.Sample.AspNet.Controllers
+{
+    [RoutePrefix("api/values")]
+    public class ValuesController : ApiController
+    {
+        [HttpGet]
+        public async Task<IHttpActionResult> Get()
+        {
+            var httpClient = new HttpClient(new HttpTracingHandler());
+            var values = await httpClient.GetStringAsync("http://localhost:5001/api/values");
+            return Json(values);
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<string> Get(int id)
+        {
+            var client = HttpClientFactory.Create(new HttpTracingHandler(null));
+            Task.WhenAll(client.GetAsync("http://localhost:5002/api/delay/2000"),
+                client.GetAsync("http://localhost:5002/api/values"),
+                client.GetAsync("http://localhost:5002/api/delay/200"));
+            return await client.GetStringAsync("http://localhost:5002/api/delay/100");
+        }
+    }
+}
