6e4ff6b219a1853a5bbe9c6e58e11262b15e5851
Andyliyuze <874787504@qq.com>
2021-01-14 15:52:41 +0800

    Add MongoDB Diagnostics (#369)

---
+++ src/SkyApm.Diagnostics.MongoDB/DiagnosticsActivityEventSubscriber.cs
@@ -0,0 +1,143 @@
+﻿/*
+ * Licensed to the SkyAPM under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The SkyAPM licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+using MongoDB.Driver.Core.Events;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace SkyApm.Diagnostics.MongoDB
+{
+    public class DiagnosticsActivityEventSubscriber : IEventSubscriber
+    {
+        public static DiagnosticSource diagnosticSource = new DiagnosticListener("MongoSourceListener");
+        internal static readonly AssemblyName AssemblyName = typeof(DiagnosticsActivityEventSubscriber).Assembly.GetName();
+        internal static readonly string ActivitySourceName = AssemblyName.Name;
+        internal static readonly Version Version = AssemblyName.Version;
+         
+        private readonly ReflectionEventSubscriber _subscriber;
+        private readonly ConcurrentDictionary<int, Activity> _activityMap = new ConcurrentDictionary<int, Activity>();
+
+        private static readonly HashSet<string> CommandsWithCollectionNameAsValue =
+            new HashSet<string>
+            {
+                "aggregate",
+                "count",
+                "distinct",
+                "mapReduce",
+                "geoSearch",
+                "delete",
+                "find",
+                "killCursors",
+                "findAndModify",
+                "insert",
+                "update",
+                "create",
+                "drop",
+                "createIndexes",
+                "listIndexes"
+            };
+
+        public DiagnosticsActivityEventSubscriber()
+        { 
+            _subscriber = new ReflectionEventSubscriber(this, bindingFlags: BindingFlags.Instance | BindingFlags.NonPublic);
+        }
+
+        public bool TryGetEventHandler<TEvent>(out Action<TEvent> handler)
+            => _subscriber.TryGetEventHandler(out handler);
+
+        private void Handle(CommandStartedEvent @event)
+        {
+            var collectionName = GetCollectionName(@event);
+            if (string.IsNullOrWhiteSpace(collectionName))
+            {
+                return;
+            }
+            var activity = new Activity("MongoActivity");
+            if (activity == null)
+            {
+                return;
+            }
+            _activityMap.TryAdd(@event.RequestId, activity);
+            diagnosticSource.StartActivity(activity, @event);
+        }
+
+        private void Handle(CommandSucceededEvent @event)
+        {
+            if (_activityMap.TryRemove(@event.RequestId, out var activity))
+            {
+                WithReplacedActivityCurrent(activity, () =>
+                {
+                    diagnosticSource.StopActivity(activity, @event);
+                });
+            }
+        }
+
+        private void Handle(CommandFailedEvent @event)
+        {
+            if (_activityMap.TryRemove(@event.RequestId, out var activity))
+            {
+                WithReplacedActivityCurrent(activity, () =>
+                {                    
+                    diagnosticSource.Write("MongoActivity.Failed", @event);                  
+                });
+            }
+        }
+
+        public static string GetCollectionName(CommandStartedEvent @event)
+        {
+            if (@event.CommandName == "getMore")
+            {
+                if (@event.Command.Contains("collection"))
+                {
+                    var collectionValue = @event.Command.GetValue("collection");
+                    if (collectionValue.IsString)
+                    {
+                        return collectionValue.AsString;
+                    }
+                }
+            }
+            else if (CommandsWithCollectionNameAsValue.Contains(@event.CommandName))
+            {
+                var commandValue = @event.Command.GetValue(@event.CommandName);
+                if (commandValue != null && commandValue.IsString)
+                {
+                    return commandValue.AsString;
+                }
+            }
+
+            return null;
+        }
+
+        private static void WithReplacedActivityCurrent(Activity activity, Action action)
+        {
+            var current = Activity.Current;
+            try
+            {
+                Activity.Current = activity;
+                action();
+            }
+            finally
+            {
+                Activity.Current = current;
+            }
+        }
+    }
+}
---
+++ src/SkyApm.Diagnostics.MongoDB/MongoDiagnosticProcessor.cs
@@ -0,0 +1,77 @@
+﻿/*
+ * Licensed to the SkyAPM under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The SkyAPM licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+using MongoDB.Driver.Core.Events;
+using SkyApm.Tracing;
+using System;
+
+namespace SkyApm.Diagnostics.MongoDB
+{
+    public class MongoDiagnosticsProcessor : ITracingDiagnosticProcessor
+    {
+        public string ListenerName => "MongoSourceListener";
+        private readonly ITracingContext _tracingContext;
+        private readonly IExitSegmentContextAccessor _contextAccessor;
+ 
+        public MongoDiagnosticsProcessor(ITracingContext tracingContext,
+            IExitSegmentContextAccessor contextAccessor)
+        {
+            _tracingContext = tracingContext;
+            _contextAccessor = contextAccessor;
+        }
+
+        [DiagnosticName("MongoActivity.Start")]
+        public void BeforeExecuteCommand([Object] CommandStartedEvent @event)
+        {
+            var operationName = DiagnosticsActivityEventSubscriber.GetCollectionName(@event);
+            var context = _tracingContext.CreateExitSegmentContext(operationName, @event.ConnectionId.ServerId.EndPoint.ToString());
+            context.Span.SpanLayer = Tracing.Segments.SpanLayer.DB;
+            context.Span.Component = Common.Components.MongoDBCLIENT;
+            context.Span.AddTag("db.system", "mongodb");
+            context.Span.AddTag("db.name", @event.DatabaseNamespace?.DatabaseName);
+            context.Span.AddTag("db.mongodb.collection", operationName);
+            context.Span.AddTag("db.operation", operationName + @event.CommandName);
+            context.Span.AddTag(Common.Tags.DB_TYPE, "sql");
+            context.Span.AddTag(Common.Tags.DB_INSTANCE, @event.DatabaseNamespace.DatabaseName);
+            context.Span.AddTag(Common.Tags.DB_STATEMENT, @event.Command.ToString());
+        }
+
+        [DiagnosticName("MongoActivity.Stop")]
+        public void AfterExecuteCommand([Object] CommandSucceededEvent @event)
+        { 
+            var context = _contextAccessor.Context;
+            context?.Span.AddTag(Common.Tags.STATUS_CODE, "ok");
+
+            _tracingContext.Release(context);
+        }
+
+        [DiagnosticName("MongoActivity.Failed")]
+        public void FailedExecuteCommand([Object] CommandFailedEvent @event)
+        {
+            var context = _contextAccessor.Context;
+            context?.Span.AddTag("status_description", @event.Failure.Message);
+            context?.Span.AddTag("error.type", @event.Failure.GetType().FullName);
+            context?.Span.AddTag("error.msg", @event.Failure.Message);
+            context?.Span.AddTag("error.stack", @event.Failure.StackTrace);
+            context?.Span.AddTag(Common.Tags.STATUS_CODE, "error");
+
+            _tracingContext.Release(context);
+        }
+         
+    }
+}
