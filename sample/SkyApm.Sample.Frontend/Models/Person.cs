05f115c6a9a8f4383049d8fce4dce3bb11f232b0
郭刚平 <gangping.guo@xiaobao100.com>
2021-03-17 16:50:17 +0800

    feature: Support collect request body for AspNetCore.

---
+++ sample/SkyApm.Sample.Frontend/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SkyApm.Sample.Frontend.Models
+{
+    public class Person
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
--- src/SkyApm.Diagnostics.AspNetCore/Config/HostingDiagnosticConfig.cs
+++ src/SkyApm.Diagnostics.AspNetCore/Config/HostingDiagnosticConfig.cs
@@ -17,5 +17,15 @@ namespace SkyApm.Diagnostics.AspNetCore.Config
         /// Auto collect specific headers as span tag
         /// </summary>
         public List<string> CollectHeaders { get; set; }
+
+        /// <summary>
+        /// Auto collect request body when Content-Type meets
+        /// </summary>
+        public List<string> CollectBodyContentTypes { get; set; }
+
+        /// <summary>
+        /// Request body will skip collecting if the Content-Length is larger than this value. 
+        /// </summary>
+        public int CollectBodyLengthThreshold { get; set; } = 2048;
     }
 }
