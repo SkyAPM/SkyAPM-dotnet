580cf2e45450ef53edf084d6e4a153d130870fd1
泽威 王 <1585955375@qq.com>
2022-04-27 21:41:53 +0800

    feat : add a copyright header to all new files

--- sample/Sky.Apm.Sample.Logging/Controllers/WeatherForecastController.cs
+++ sample/Sky.Apm.Sample.Logging/Controllers/WeatherForecastController.cs
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the SkyAPM under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The SkyAPM licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 using Microsoft.AspNetCore.Mvc;
 using SkyApm.Tracing;
 
@@ -24,7 +42,7 @@ namespace Sky.Apm.Sample.Logging.Controllers
         public IEnumerable<WeatherForecast> Get()
         {
             //Console.WriteLine(_entrySegmentContextAccessor.Context?.TraceId);
-            _loggerFactory.Information("qiwnkasdnaskndlaknd laksndk asndkas ndl nsald nlasdn la£¡£¡£¡£¡£¡£¡£¡£¡£¡£¡£¡£¡");
+            _loggerFactory.Information("qiwnkasdnaskndlaknd laksndk asndkas ndl nsald nlasdn laï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½");
             _test.Create();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
