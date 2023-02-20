bf35388432bda9f838499a307b4ec82566cf8961
ihourglass <inversionhourglass@outlook.com>
2023-02-20 15:00:43 +0800

    Support access to ITracingContext through static property.

---
+++ src/SkyApm.Utilities.StaticAccessor/NullConfigAccessor.cs
@@ -0,0 +1,11 @@
+﻿using SkyApm.Config;
+
+namespace SkyApm.Utilities.StaticAccessor
+{
+    internal class NullConfigAccessor : IConfigAccessor
+    {
+        public T Get<T>() where T : class, new() => new T();
+
+        public T Value<T>(string key, params string[] sections) => default;
+    }
+}
---
+++ src/SkyApm.Utilities.StaticAccessor/SkyApmInstances.cs
@@ -0,0 +1,12 @@
+﻿using SkyApm.Config;
+using SkyApm.Tracing;
+
+namespace SkyApm.Utilities.StaticAccessor
+{
+    public static class SkyApmInstances
+    {
+        public static ITracingContext TracingContext { get; internal set; } = new NullTracingContext();
+
+        public static IConfigAccessor ConfigAccessor { get; internal set; } = new NullConfigAccessor();
+    }
+}
