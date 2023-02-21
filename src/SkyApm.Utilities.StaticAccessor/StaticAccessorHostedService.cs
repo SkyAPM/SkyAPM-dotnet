7e7693dd7ad692f2e52037232dc741dad6a71cd4
inversionhourglass <inversionhourglass@outlook.com>
2023-02-21 14:02:15 +0800

    Support access to ITracingContext through static property. (#540)
    * Support access to ITracingContext through static property.
    
    * SkyApm.Utilities.StaticAccessor support net7.0
    
    * Add license to code files of project SkyApm.Utilities.StaticAccessor.
    
    * Try resolve ITracingContext from IServiceProvider not necessary in StaticAccessorHostedService.

---
+++ src/SkyApm.Utilities.StaticAccessor/StaticAccessorHostedService.cs
@@ -0,0 +1,56 @@
+﻿/*
+ * Licensed to the SkyAPM under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The SkyAPM licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SkyApm.Config;
+using SkyApm.Tracing;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SkyApm.Utilities.StaticAccessor
+{
+    internal class StaticAccessorHostedService : IHostedService
+    {
+        private readonly ITracingContext _tracingContext;
+        private readonly IConfigAccessor _configAccessor;
+
+        public StaticAccessorHostedService(IServiceProvider provider)
+        {
+            _tracingContext = provider.GetService<ITracingContext>();
+            _configAccessor = provider.GetService<IConfigAccessor>();
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            if (_tracingContext != null && _configAccessor != null)
+            {
+                SkyApmInstances.TracingContext = _tracingContext;
+                SkyApmInstances.ConfigAccessor = _configAccessor;
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
