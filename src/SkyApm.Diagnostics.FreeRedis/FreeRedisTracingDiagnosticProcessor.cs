938cfd575d5e752a798c0f0b0702252c3309aa77
guochen2 <524014724@qq.com>
2023-03-08 11:29:45 +0800

    Add FreeRedis Diagnostics

---
+++ src/SkyApm.Diagnostics.FreeRedis/FreeRedisTracingDiagnosticProcessor.cs
@@ -0,0 +1,65 @@
+﻿using FreeRedis;
+using SkyApm;
+using SkyApm.Common;
+using SkyApm.Config;
+using SkyApm.Diagnostics;
+using SkyApm.Tracing;
+using SkyApm.Tracing.Segments;
+using System;
+using System.Linq;
+
+namespace SkyApm.Diagnostics.FreeRedis
+{
+
+
+    /// <summary>
+    /// FreeRedisTracingDiagnosticProcessor
+    /// </summary>
+    public class FreeRedisTracingDiagnosticProcessor : ITracingDiagnosticProcessor
+    {
+        public static readonly StringOrIntValue Free_Redis = new StringOrIntValue(7, "Redis");
+
+        #region Const
+
+        public const string ComponentName = "FreeRedis";
+
+        public const string FreeRedis_Notice = "FreeRedis.Notice";
+
+        #endregion
+
+        public string ListenerName => "FreeRedisDiagnosticListener";
+
+        private readonly ITracingContext _tracingContext;
+        private readonly ILocalSegmentContextAccessor _localSegmentContextAccessor;
+        private readonly TracingConfig _tracingConfig;
+        public FreeRedisTracingDiagnosticProcessor(ITracingContext tracingContext,
+            ILocalSegmentContextAccessor localSegmentContextAccessor, IConfigAccessor configAccessor)
+        {
+            _tracingContext = tracingContext;
+            _localSegmentContextAccessor = localSegmentContextAccessor;
+            _tracingConfig = configAccessor.Get<TracingConfig>();
+        }
+
+        private SegmentContext CreateFreeRedisLocalSegmentContext(string operation)
+        {
+            var context = _tracingContext.CreateLocalSegmentContext(operation);
+            context.Span.SpanLayer = SpanLayer.CACHE;
+            context.Span.Component = Free_Redis;
+            context.Span.AddTag("cache.type", "FreeRedis");
+            return context;
+        }
+
+        #region Notice
+        [DiagnosticName(FreeRedis_Notice)]
+        public void Notice([Object] NoticeEventArgs eventData)
+        {
+            var context = CreateFreeRedisLocalSegmentContext(eventData.NoticeType.ToString());
+            context.Span.AddTag("cache.statement", eventData.Log);
+            if (eventData?.Exception != null)
+                context.Span.ErrorOccurred(eventData.Exception, _tracingConfig);
+            _tracingContext.Release(context);
+
+        }
+        #endregion
+    }
+}
---
+++ src/SkyApm.Diagnostics.FreeRedis/SkyWalkingBuilderExtensions.cs
@@ -0,0 +1,41 @@
+﻿using FreeRedis;
+using Microsoft.Extensions.DependencyInjection;
+using SkyApm;
+using SkyApm.Utilities.DependencyInjection;
+using System;
+using System.Diagnostics;
+
+namespace SkyApm.Diagnostics.FreeRedis
+{
+
+    public static class SkyWalkingBuilderExtensions
+    {
+        public static readonly DiagnosticListener dl = new DiagnosticListener("FreeRedisDiagnosticListener");
+
+        public static SkyApmExtensions AddFreeRedis(this SkyApmExtensions extensions, RedisClient redisClient, bool includeAuth = false)
+        {
+            if (extensions == null)
+            {
+                throw new ArgumentNullException(nameof(extensions));
+            }
+            extensions.Services.AddSingleton<ITracingDiagnosticProcessor, FreeRedisTracingDiagnosticProcessor>();
+            if (redisClient != null)
+            {
+                ConfigAop(redisClient, includeAuth);
+            }
+            return extensions;
+        }
+
+
+        public static void ConfigAop(RedisClient redisClient, bool includeAuth = false)
+        {
+            redisClient.Notice += (s, e) =>
+            {
+                if (!string.IsNullOrWhiteSpace(e.Log) && (includeAuth || (!e.Log.Contains("> AUTH "))))
+                {
+                    dl.Write(FreeRedisTracingDiagnosticProcessor.FreeRedis_Notice, e);
+                }
+            };
+        }
+    }
+}
