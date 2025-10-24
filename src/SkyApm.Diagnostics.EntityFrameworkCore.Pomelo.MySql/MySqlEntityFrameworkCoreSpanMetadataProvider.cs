6286cf9edc0e8addbfbd07a5350efe49cf0ca696
James <shunjiey@hotmail.com>
2025-10-24 14:48:00 +0800

    Optimize the Match method of MySqlEntityFrameworkCoreSpanMetadataProvider to support the MySqlConnector connection type

--- src/SkyApm.Diagnostics.EntityFrameworkCore.Pomelo.MySql/MySqlEntityFrameworkCoreSpanMetadataProvider.cs
+++ src/SkyApm.Diagnostics.EntityFrameworkCore.Pomelo.MySql/MySqlEntityFrameworkCoreSpanMetadataProvider.cs
@@ -35,7 +35,8 @@ namespace SkyApm.Diagnostics.EntityFrameworkCore
         
         public bool Match(DbConnection connection)
         {
-            return connection.GetType().FullName == "MySql.Data.MySqlClient.MySqlConnection";
+            var type = connection.GetType().FullName;
+            return type == "MySql.Data.MySqlClient.MySqlConnection" || type == "MySqlConnector.MySqlConnection";
         }
 
         public string GetPeer(DbConnection connection)
